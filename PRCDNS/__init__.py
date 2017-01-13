4286242a5cc1eec8bd2b271799bffd60bc32ccd2
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-01-13 14:21:45 +0800

    init Packaging

--- PRCDNS/__init__.py
+++ PRCDNS/__init__.py
@@ -1,3 +1,12 @@
+from PRCDNS import chinaz_client
+
+
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
+    client = chinaz_client.ChinazClient()
+    client.query_domain()
+
+
+if __name__ == "__main__":
+    main()
---
+++ PRCDNS/chinaz_client.py
@@ -0,0 +1,4 @@
+class ChinazClient:
+    """客户端 查询站长之家域名是否已经备案"""
+    def query_domain(self):
+        print('query')
