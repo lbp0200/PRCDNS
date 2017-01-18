2cd8d9b36a1d19f196fc8bbd81e7077dcf829c5d
lbp <lbp@lbp.im>
2017-01-18 23:49:33 +0800

    dns-over-http

--- PRCDNS/__init__.py
+++ PRCDNS/__init__.py
@@ -6,13 +6,13 @@ import async_timeout
 
 async def fetch(session, url):
     with async_timeout.timeout(10):
-        async with session.get(url) as response:
+        async with session.get(url,proxy="http://127.0.0.1:8118") as response:
             return await response.text()
 
 
 async def start(loop):
     async with aiohttp.ClientSession(loop=loop) as session:
-        html = await fetch(session, 'http://127.0.0.1/')
+        html = await fetch(session, 'https://dns.google.com/resolve?name=img.alicdn.com&edns_client_subnet=223.72.90.21/24')
         print(html)
 
 
