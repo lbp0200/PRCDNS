3f1b16c8ef68eb3dbf1849923ad1716bbd6e2f97
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-01-22 18:11:20 +0800

    rm rocksdb

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Faker'],
+    install_requires=['asyncio', 'aiohttp', 'aiodns', 'cchardet', 'dnslib'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -84,7 +84,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-         'prcdns': ['domain_data.dat'],
+        'prcdns': ['domain_data.dat'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
