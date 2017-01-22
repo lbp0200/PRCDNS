24272b0da99d13f14c184a944f9760244b1e6161
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-01-22 17:50:10 +0800

    rm rocksdb

--- PRCDNS/__init__.py
+++ PRCDNS/__init__.py
@@ -1,13 +1,16 @@
 import asyncio
 import json
-from PRCDNS.proxy_client import ProxyClient
+
 from dnslib import *
+
+from PRCDNS.proxy_client import ProxyClient
 
 
 class EchoServerClientProtocol(asyncio.Protocol):
     gFuncs = globals()
 
     def get_data(self, data):
+        print(data)
         sz = struct.unpack(">H", data[:2])[0]
         if sz < len(data) - 2:
             raise Exception("Wrong size of TCP packet")
@@ -25,7 +28,14 @@ class EchoServerClientProtocol(asyncio.Protocol):
         request = DNSRecord.parse(data)
         print('Data received: {!r}'.format(request))
 
-        google_dns_resp = """{"Status": 0,"TC": false,"RD": true,"RA": true,"AD": false,"CD": false,"Question":[ {"name": "img.alicdn.com.","type": 1}],"Answer":[ {"name": "img.alicdn.com.","type": 5,"TTL": 85493,"data": "img.alicdn.com.danuoyi.alicdn.com."},{"name": "img.alicdn.com.danuoyi.alicdn.com.","type": 1,"TTL": 59,"data": "111.32.130.108"},{"name": "img.alicdn.com.danuoyi.alicdn.com.","type": 1,"TTL": 59,"data": "111.32.130.109"}],"Additional":[],"edns_client_subnet": "223.72.90.0/24","Comment": "Response from danuoyinewns3.gds.alicdn.com.(121.42.1.129)"}"""
+        loop1 = asyncio.new_event_loop()
+        client = ProxyClient()
+        # coros = asyncio.gather(*tasks)
+        url = 'https://dns.google.com/resolve?name={}&edns_client_subnet=223.72.90.21/24'.format(str(request.q.qname))
+        google_dns_resp = loop1.run_until_complete(client.get(loop1, url))
+        print(google_dns_resp)
+        loop1.close()
+        # google_dns_resp = """{"Status": 0,"TC": false,"RD": true,"RA": true,"AD": false,"CD": false,"Question":[ {"name": "img.alicdn.com.","type": 1}],"Answer":[ {"name": "img.alicdn.com.","type": 5,"TTL": 85493,"data": "img.alicdn.com.danuoyi.alicdn.com."},{"name": "img.alicdn.com.danuoyi.alicdn.com.","type": 1,"TTL": 59,"data": "111.32.130.108"},{"name": "img.alicdn.com.danuoyi.alicdn.com.","type": 1,"TTL": 59,"data": "111.32.130.109"}],"Additional":[],"edns_client_subnet": "223.72.90.0/24","Comment": "Response from danuoyinewns3.gds.alicdn.com.(121.42.1.129)"}"""
         resp = json.loads(google_dns_resp)
         a = request.reply()
         for answer in resp['Answer']:
@@ -42,16 +52,9 @@ class EchoServerClientProtocol(asyncio.Protocol):
 
 
 def main():
-    # print(locals()['CNAME']('img.alicdn.com.danuoyi.alicdn.com.'))
     loop = asyncio.get_event_loop()
-    client = ProxyClient()
-    loop.run_until_complete( client.query_domain(loop))
-    print(
-
-    )
-    exit()
     # Each client connection will create a new protocol instance
-    coro = loop.create_server(EchoServerClientProtocol, '127.0.0.1', 5353)
+    coro = loop.create_server(lambda: EchoServerClientProtocol(), '127.0.0.1', 5353)
     server = loop.run_until_complete(coro)
 
     try:
@@ -61,6 +64,7 @@ def main():
 
     print('Close Server')
     server.close()
+    loop.run_until_complete(server.wait_closed())
     loop.close()
 
 
