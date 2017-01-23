36ad4e0e11acebd80ce70f126b0344af2f08e2fc
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-01-23 17:23:21 +0800

    完成，并尝试新增单元测试

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,13 @@
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
+import unittest
 
 
-def test_success():
-    assert True
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
