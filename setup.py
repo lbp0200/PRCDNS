5de0157da2cb5506f8f9a5b8090554c635a38f67
刘博平(焦点) <bopingliu@sohu-inc.com>
2017-03-14 10:12:52 +0800

    更新IP获取接口

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.7',
+    version='0.0.8',
 
     description='准确、CDN友好的DNS软件，使用DNS-Over-HTTP',
     long_description=long_description,
